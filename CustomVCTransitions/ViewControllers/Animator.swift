3fa5386b99c409629e50385d5a229ffb3746a91a
Alex Rybachev <rybachev.as@gmail.com>
2022-06-24 21:30:34 +0400

    Adding a fading background

--- CustomVCTransitions/ViewControllers/Animator.swift
+++ CustomVCTransitions/ViewControllers/Animator.swift
@@ -58,6 +58,10 @@ final class Animator: NSObject, UIViewControllerAnimatedTransitioning {
         
         let isPresenting = type.isPresenting
         
+        let backgroundView: UIView
+        let fadeView = UIView(frame: containerView.bounds)
+        fadeView.backgroundColor = secondViewController.view.backgroundColor
+        
 //        let imageViewSnapshot: UIView
 //
 //        if isPresenting {
@@ -68,13 +72,20 @@ final class Animator: NSObject, UIViewControllerAnimatedTransitioning {
         
         if isPresenting {
             selectedCellImageViewSnapshot = cellImageSnapshot
+            
+            backgroundView = UIView(frame: containerView.bounds)
+            backgroundView.addSubview(fadeView)
+            fadeView.alpha = 0
+        } else {
+            backgroundView = firstViewController.view.snapshotView(afterScreenUpdates: true) ?? fadeView
+            backgroundView.addSubview(fadeView)
         }
         
         toView.alpha = 0
         
 //        [imageViewSnapshot].forEach { containerView.addSubview($0) }
         
-        [selectedCellImageViewSnapshot, controllerImageSnapshot].forEach { containerView.addSubview($0) }
+        [backgroundView, selectedCellImageViewSnapshot, controllerImageSnapshot].forEach { containerView.addSubview($0) }
         
         let controllerImageViewRect = secondViewController.locationImageView.convert(secondViewController.locationImageView.bounds, to: window)
         
@@ -91,7 +102,6 @@ final class Animator: NSObject, UIViewControllerAnimatedTransitioning {
         
         selectedCellImageViewSnapshot.alpha = isPresenting ? 1 : 0
         
-        
         UIView.animateKeyframes(withDuration: Self.duration, delay: 0, options: .calculationModeCubic, animations: {
             
             UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 1) {
@@ -100,11 +110,14 @@ final class Animator: NSObject, UIViewControllerAnimatedTransitioning {
                 self.selectedCellImageViewSnapshot.frame = isPresenting ? controllerImageViewRect : self.cellImageViewRect
                 controllerImageSnapshot.frame = isPresenting ? controllerImageViewRect : self.cellImageViewRect
                 
+                fadeView.alpha = isPresenting ? 1 : 0
             }
             
             UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.6) {
                 self.selectedCellImageViewSnapshot.alpha = isPresenting ? 0 : 1
                 controllerImageSnapshot.alpha = isPresenting ? 1 : 0
+                
+                backgroundView.removeFromSuperview()
             }
         }, completion: { _ in
             
